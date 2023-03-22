d74ce277b52fe7e8cc4eddda5179bfe6efd6040a
MrtnOmwenga <martin36449@gmail.com>
2023-03-22 09:00:33 -0700

    Debugging

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -19,7 +19,7 @@ class FileStorage:
         """Returns a dictionary of models currently in storage"""
         if cls is not None:
             obj_dict = {}
-            for key, value in FileStorage.__objects.items():
+            for key, value in self.__objects.items():
                 if cls == value.__class__ or cls == value.__class__.__name__:
                     obj_dict[key] = value
             return (obj_dict)
